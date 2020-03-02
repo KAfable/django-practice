9002a273bcd405d1edb63b70732ba2a096a5d219
Vyraal1 <kevin.afable@gmail.com>
2020-03-02 09:50:08 -0500

    fix: typo in field

--- pollster/polls/models.py
+++ pollster/polls/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Question(models.Model):
     # automatically creates an ID and auto increments
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('Date Oublished')
+    pub_date = models.DateTimeField('Date Published')
 
     def __str__(self):
         return self.question_text
