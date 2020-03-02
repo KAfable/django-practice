48295d040c3cca6e07df8f0c61b9b0b7c2342447
Vyraal1 <kevin.afable@gmail.com>
2020-03-02 09:49:36 -0500

    feat: adding models in admin view

--- pollster/polls/admin.py
+++ pollster/polls/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Question, Choice
 
-# Register your models here.
+
+# this is where you are able to add admin functionality
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [(None, {'fields': ['question_text']}),
+                 ('Date Information', {'fields': ['pub_date'], 'classes':['collapse']}), ]
+    inlines = [ChoiceInline]
+
+
+# on the admin view added Question and Choice CRUD
+# admin.site.register(Question)
+# admin.site.register(Choice)
+admin.site.register(Question, QuestionAdmin)
